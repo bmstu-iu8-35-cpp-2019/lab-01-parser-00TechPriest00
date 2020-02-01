617b096e65d95826bd0a7a4f2f6be700d6c038c9
00Tech-Priest00 <james.sayfer2015@yandex.ru>
2020-02-01 20:13:56 +0300

    3

--- sources/main.cpp
+++ sources/main.cpp
@@ -15,7 +15,7 @@ int main() {
         JSON a("{\"lastname\" : \"Ivanov\", \"firstname\" : \"Ivan\","\
 "\"age\" : 25, \"islegal\" : false,\"marks\" : [   4, 5, 5, 5, 2, 3] ,"\
 "\"address\" : {\"city\" : \"Moscow\", \"street\" : "\
-"\"Vozdvijenka\"}}",position, false);
+"\"Vozdvijenka\"}}", position , false);
         try {
             std::cout << std::any_cast<double>
 (std::any_cast<JSON>(a["marks"])[3]) << std::endl;
